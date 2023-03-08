91087b093e1755a404dd2cea5880bac53060401b
Yunjung Jo <j_yunj@naver.com>
2023-03-08 11:01:35 +0900

    ㅁㄱㄱㄱㄱ

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,15 +1,20 @@
 import React from 'react'
-
+import { MdEdit, MdDelete } from 'react-icons/md'
+// 리액트 아이콘 사용하기
+// npm install react-icons
 export default function note({ note }) {
-  //노트 데이터 받기
+  //노트 데이터 주기
   return (
     <li className="note">
+      {/* 원래 여기에key={note.id}가 있었음 */}
       <h4 className="title">{note.title}</h4>
       <p>{note.detail}</p>
       <div className="bottom">
         <div className="date">{note.date}</div>
-        <button>수정</button>
-        <button>삭제</button>
+        <div className="btns">
+          <MdEdit />
+          <MdDelete />
+        </div>
       </div>
     </li>
   )
--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,15 +1,23 @@
-import db from '../db/data.json'
-import React from 'react'
+// import db from '../db/data.json'
+import { useEffect, useState } from 'react'
 import Note from './Note'
 //데이터 불러오기
 
+// json fatch사용해서 데이터 불러오기 (readme 참고)
 export default function NoteList() {
+  const [notes, setNotes] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:3001/notes')
+      .then((res) => res.json())
+      .then((data) => setNotes(data))
+  }, [])
+
   return (
     <ul className="list">
-      {db.notes.map((note) => (
+      {notes.map((note) => (
         <Note note={note} key={note.id} />
+        // 원래 li note였던 것 데이터 파일 관리를 위해 note.js파일로 만들었음 노트를 태그하고 값이랑 아이디도 함께 불러와서 넘겨줌
       ))}
-      {/* 리스트 데이터 다시 note.js로 옯기기 노트 값이랑 아이디도 같이 넘기기 */}
     </ul>
   )
 }
